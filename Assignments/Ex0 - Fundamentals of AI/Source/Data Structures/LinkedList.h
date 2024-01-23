28058189b8f5957f85e3552194e151bb48a192e0
millerandrew1 <millerandrew1@ufl.edu>
2024-01-22 21:37:45 -0500

    Updates for assignment

--- Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
+++ Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
@@ -1,6 +1,204 @@
 
 
-// LinkedList class should go in the "ufl_cap4053::fundamentals" namespace!
-namespace ufl_cap4053 { namespace fundamentals {
 
-} }  // namespace ufl_cap4053::fundamentals
+// LinkedList class should go in the "ufl_cap4053::fundamentals" namespace!
+//Some resources I used to help me remember how to write a linked list and how to do an iteraotr class:
+// stackoverflow.com/questions/55402803/linked-list-iterator-implementation-c
+//  geeksforgeeks.org/introduction-and-insertion-in-a-doubly-linked-list/#
+namespace ufl_cap4053 {
+	namespace fundamentals {
+
+		template<class T>
+		class LinkedList {
+			
+			struct Node {
+				T data;
+				Node* prev;
+				Node* next;
+
+				Node(T datas) {
+					data = datas;
+					prev = nullptr;
+					next = nullptr;
+				}
+			};
+			Node* head;
+			Node* tail;
+
+		public:
+			//A TA helped me grasp the iterator class so credit to them
+			class Iterator {
+				
+				Node* iter;
+
+			public:
+				Iterator(Node* temp) {
+					iter = temp;
+				}
+
+				T operator*() const {
+					return iter->data;
+				}
+
+				Iterator& operator++() {
+					if (iter->next != nullptr) {
+						iter = iter->next;
+						return *this;
+					}
+					else {
+						iter = nullptr;
+						return *this;
+					}
+				}
+
+				bool operator==(Iterator const& rhs) {
+					if (rhs.iter == iter) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+
+				bool operator!=(Iterator const& rhs) {
+					if (rhs.iter != iter) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+			};
+
+			LinkedList<T>() {
+				head = nullptr;
+				tail = nullptr;
+			}
+
+			Iterator begin() const {
+				return Iterator(head);
+				
+			}
+
+			Iterator end() const {
+				if (tail != nullptr) {
+					return Iterator(tail->next);
+				}
+				else {
+					return Iterator(nullptr);
+				}
+			}
+
+			bool isEmpty() const {
+				if (head == nullptr) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+
+			T getFront() const {
+				return head->data;
+			}
+
+			T getBack() const {
+				return tail->data;
+			}
+
+			void enqueue(T element) {
+				Node* temp = new Node(element);
+				if (head == nullptr) {
+					head = temp;
+					tail = temp;
+				}
+				else {
+					tail->next = temp;
+					temp->prev = tail;
+					tail = temp;
+				}
+			}
+
+			void dequeue() {
+				if (head != nullptr) {
+					Node* temp = head;
+					head = head->next;
+					
+					if (head == nullptr) {
+						tail = head;
+					}
+					else {
+						head->prev = nullptr;
+					}
+					delete temp;
+				}
+			}
+
+			void pop() {
+				if (tail->data != nullptr) {
+					Node* temp = tail;
+					tail = tail->prev;
+					
+					if (tail == nullptr) {
+						head = tail;
+					}
+					else {
+						tail->next = nullptr;
+					}
+					delete temp;
+				}
+			}
+
+			void clear() {
+				Node* temp = head;
+				while (temp != nullptr) {
+					Node* follow = temp;
+					temp = temp->next;
+					delete follow;
+				}
+				head = nullptr;
+				tail = nullptr;
+			}
+
+			bool contains(T element) const {
+				Node* temp = head;
+				while (temp != nullptr) {
+					if (temp->data == element) {
+						return true;
+					}
+					temp = temp->next;
+				}
+				return false;
+			}
+
+			void remove(T element) {
+				Node* temp = head;
+				while (temp != nullptr) {
+					if (temp->data == element) {
+
+						if (temp->prev != nullptr) {
+							temp->prev->next = temp->next;
+						}
+						else {
+							head = head->next;
+						}
+
+						if (temp->next != nullptr) {
+							temp->next->prev = temp->prev;
+						}
+						else {
+							tail = temp->prev;
+						}
+						delete temp;
+						break;
+					}
+					temp = temp->next;
+				}
+			}
+
+
+		};
+
+
+	}
+}  // namespace ufl_cap4053::fundamentals
--- Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/TreeNode.h
+++ Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/TreeNode.h
@@ -1,6 +1,156 @@
 
 
 // TreeNode class should go in the "ufl_cap4053::fundamentals" namespace!
+//Used this to help formulate how to build the treenode class: 
+// www.geeksforgeeks.org/generic-treesn-array-trees/
+// www.studytonight.com/advanced-data-structures/nary-tree
 namespace ufl_cap4053 { namespace fundamentals {
 
+	template <class T>
+	class TreeNode {
+	public:
+		T data;
+		TreeNode<T>* currChild;
+		TreeNode<T>* sibling;
+
+		
+		
+		TreeNode() {
+			data = nullptr;
+			currChild = nullptr;
+			sibling = nullptr;
+		}
+
+		TreeNode(T datas) {
+			data = datas;
+			currChild = nullptr;
+			sibling = nullptr;
+		}
+
+		const T& getData() const {
+			return data;
+		}
+
+		size_t getChildCount() const {
+			size_t num = 0;
+			TreeNode* temp = currChild;
+			
+			if (currChild == nullptr) {
+				return num;
+			}
+			num++;
+			
+			while (temp->sibling != nullptr) {
+				num++;
+				temp = temp->sibling;
+			}
+			return num;
+
+		}
+
+		TreeNode<T>* getChild(size_t index) {
+			if (index == 0) {
+				return currChild;
+			}
+			else {
+				TreeNode* temp = currChild;
+				size_t num = 0;
+				while (num != index) {
+					if (temp->sibling != nullptr) {
+						num++;
+						temp = temp->sibling;
+					}
+					else {
+						return nullptr;
+					}
+				
+				}
+				return temp;
+			}
+		}
+
+		TreeNode<T>* getChild(size_t index) const{
+			if (index == 0) {
+				return currChild;
+			}
+			else {
+				TreeNode* temp = currChild;
+				size_t num = 0;
+				while (num != index) {
+					if (temp->sibling != nullptr) {
+						num++;
+						temp = temp->sibling;
+					}
+					else {
+						return nullptr;
+					}
+
+				}
+				return temp;
+			}
+		}
+
+		void addChild(TreeNode<T>* child) {
+			if (currChild == nullptr) {
+				currChild = child;
+			}
+			else {
+				TreeNode* temp = currChild;
+				while (temp->sibling != nullptr)
+				{
+					temp = temp->sibling;
+				}
+				temp->sibling = child;
+			}
+		}
+
+		TreeNode<T>* removeChild(size_t index) {
+			TreeNode* temp = currChild;
+
+			if (index == 0) {
+				
+				if (temp != nullptr) {
+					currChild = currChild->sibling;
+					temp->sibling = nullptr;
+				}
+				return temp;
+			}
+			else {
+				size_t currIndex = 0;
+
+				while (currIndex != index-1) {
+					if (temp != nullptr) {
+						temp = temp->sibling;
+						currIndex++;
+					}
+					else {
+						break;
+					}
+				}
+				if (temp->sibling == nullptr) {
+					return nullptr;
+				}
+				else {
+					TreeNode* newTemp = temp->sibling;
+					temp->sibling = newTemp->sibling;
+					newTemp->sibling = nullptr;
+					return newTemp;
+				}
+
+			}
+		}
+
+		void breadthFirstTraverse(void (*dataFunction)(const T)) const {
+
+		}
+
+		void preOrderTraverse(void (*dataFunction)(const T)) const {
+
+		}
+
+		void postOrderTraverse(void (*dataFunction)(const T)) const {
+
+		}
+
+	};
 }}  // namespace ufl_cap4053::fundamentals
